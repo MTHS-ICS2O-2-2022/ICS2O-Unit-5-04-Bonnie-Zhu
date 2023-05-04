3079dbbdcfa6266dd941ff2119a474f61c0abc92
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-05-04 17:23:36 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Bonnie Zhu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+
+function calculate() {
+  let inputedAge = document.getElementById("age").value
+  let inputedDay = document.getElementById("day").value
+
+  if (
+    inputedDay == "Tuesday" ||
+    inputedDay == "Thursday" ||
+    (inputedAge > 12 && inputedAge < 21)
+  ) {
+    document.getElementById("answer").innerHTML =
+      "You're able to get the discount!"
+  } else {
+    document.getElementById("answer").innerHTML =
+      "Sorry, you must pay for regular pricing."
+  }
+}
